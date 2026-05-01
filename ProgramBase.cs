0193ffe803817e5f04110ac56567090dbbe667ea
TIÊN TRẦN NGUYỆT THỦY <tientran.31251024604@st.ueh.edu.vn>
2026-05-01 22:37:35 +0700

    Add files via upload

---
+++ ProgramBase.cs
@@ -0,0 +1,53 @@
+﻿internal class ProgramBase
+{
+    static void Main()
+    {
+        Console.WriteLine("17 Check Number in Array ---");
+
+
+        int[] nums = [1, 5, 8, 10, 25, 30];
+        Console.WriteLine("Array: [{0}]", string.Join(", ", nums));
+
+
+        Console.Write("Enter a number to search: ");
+        int searchNum = Convert.ToInt32(Console.ReadLine());
+
+        bool exists = nums.Contains(searchNum);
+
+        if (exists)
+        {
+            Console.WriteLine($"The number {searchNum} is present in the array.");
+        }
+        else
+        {
+            Console.WriteLine($"The number {searchNum} is NOT present in the array.");
+        }
+
+        Console.ReadLine();
+    }
+    static void main()
+    {
+        Console.WriteLine("17 Check Number in Array ---");
+
+
+        int[] nums = [1, 5, 8, 10, 25, 30];
+        Console.WriteLine("Array: [{0}]", string.Join(", ", nums));
+
+
+        Console.Write("Enter a number to search: ");
+        int searchNum = Convert.ToInt32(Console.ReadLine());
+
+        bool exists = nums.Contains(searchNum);
+
+        if (exists)
+        {
+            Console.WriteLine($"The number {searchNum} is present in the array.");
+        }
+        else
+        {
+            Console.WriteLine($"The number {searchNum} is NOT present in the array.");
+        }
+
+        Console.ReadLine();
+    }
+}
